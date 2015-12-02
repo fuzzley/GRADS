6e85f441ee97191ff05ca8b9d39576a0ca7927bf
Mingxiang Zhu <mxiangzhu@gmail.com>
2015-12-02 11:02:33 -0500

    some test code

---
+++ src/edu/sc/csce740/test/GRADSTest.java
@@ -0,0 +1,91 @@
+package edu.sc.csce740.test;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import edu.sc.csce740.*;
+import edu.sc.csce740.exception.NoUserSetInSessionException;
+import edu.sc.csce740.exception.UserNotFoundException;
+import edu.sc.csce740.model.ProgressSummary;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class GRADSTest {
+	private GRADS grads;
+	private String csUser = "ggay";
+	
+	@Before
+	public void setUp() throws UserNotFoundException{
+		this.grads = new GRADS();
+		this.grads.setUser(this.csUser);
+	}
+	
+	@Test
+	public void testGenerateProgressSummary_passedPhD() throws Exception{
+		String recordsStr = null;
+		BufferedReader br = null;
+		
+		//read correct progress summary from json file
+		String fileName = null;
+		String studentId = null;
+		
+		br = new BufferedReader(new FileReader(fileName));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+		while (line != null) {
+			sb.append(line);
+			sb.append(System.lineSeparator());
+			line = br.readLine();
+		}
+		recordsStr = sb.toString();
+		br.close();
+		
+		Gson gson = new Gson();
+		ProgressSummary ps = gson.fromJson(recordsStr, ProgressSummary.class);
+		
+		ProgressSummary generated = grads.generateProgressSummary(studentId);
+		
+		assertEquals(ps, generated);
+	}
+	
+	@Test
+	public void testGenerateProgressSummary_notPassedPhD() throws Exception{
+		String recordsStr = null;
+		BufferedReader br = null;
+		
+		//read correct progress summary from json file
+		String fileName = null;
+		String studentId = null;
+		
+		br = new BufferedReader(new FileReader(fileName));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+		while (line != null) {
+			sb.append(line);
+			sb.append(System.lineSeparator());
+			line = br.readLine();
+		}
+		recordsStr = sb.toString();
+		br.close();
+		
+		Gson gson = new Gson();
+		ProgressSummary ps = gson.fromJson(recordsStr, ProgressSummary.class);
+		
+		ProgressSummary generated = grads.generateProgressSummary(studentId);
+		
+		assertEquals(ps, generated);
+	}
+	
+	
+	
+	@After
+	public void tearDown() throws NoUserSetInSessionException{
+		this.grads.clearSession();
+		this.grads = null;
+	}
+
+}
