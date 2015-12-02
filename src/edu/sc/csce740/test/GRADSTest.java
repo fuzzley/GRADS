18355f12cbebcf507aaf1b027392dcc95a9d005c
fuzzley <fuzz357@hotmail.com>
2015-12-02 10:58:00 -0500

    Started working on test cases using JUnit.

---
+++ src/edu/sc/csce740/test/GRADSTest.java
@@ -0,0 +1,46 @@
+package edu.sc.csce740.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.sc.csce740.GRADS;
+import edu.sc.csce740.exception.CoursesNotLoadedException;
+
+public class GRADSTest {
+
+	private static String coursesPath = "src/resources/courses.json";
+	private static String studentRecordsPath = "src/resources/students.json";
+	private static String usersPath = "src/resources/users.json";
+	
+	@Test
+	public void testSetUserValid() {
+		GRADS grads = new GRADS();
+
+		try {
+			grads.loadCourses(coursesPath);
+			grads.loadRecords(studentRecordsPath);
+			grads.loadUsers(usersPath);
+			
+			grads.setUser("ggay");
+		} catch (Exception ex) { }
+		
+		assert(grads.getUser() == "ggay");
+	}
+
+	@Test
+	public void testSetUserInvalid() {
+		GRADS grads = new GRADS();
+		
+		try {
+			grads.loadCourses(coursesPath);
+			grads.loadRecords(studentRecordsPath);
+			grads.loadUsers(usersPath);
+			
+			grads.setUser("gggay");
+		} catch (Exception ex) { }
+		
+		assert(grads.getUser() == null);
+	}
+
+}
